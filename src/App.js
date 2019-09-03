d9cfa2956e12cfc45a3894fe823f98c8c3cd62c0
Daniel Jones <daniel.jones2000@gmail.com>
2019-09-02 21:34:46 -0700

    created array

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
-import React from 'react';
+import React from "react";
+import TodoList from "./components/TodoComponents/TodoList";
+import TodoForm from "./components/TodoComponents/TodoForm";
+
+const todoData = [
+  {
+    task: "Organize Garage",
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: "Bake Cookies",
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
